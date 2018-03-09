3a2caccf530c5e9a09fe8a42eb79db3fc7a3e650
DavidSerre <aliidu6@hotmail.fr>
2018-03-09 14:41:57 +0100

    sin, cos et tan

---
+++ Calculator/src/fr/imie/Calculator/Launcher/tan.java
@@ -0,0 +1,19 @@
+package fr.imie.Calculator.Launcher;
+
+import java.util.Scanner;
+
+public class tan {
+	
+	 public static void main(String args[]) {
+		   
+		  Scanner user = new Scanner(System.in); 
+
+	      System.out.println("Veuillez saisir des degrees :"); 
+	      
+	      double ent = user.nextDouble(); 
+	      double radian = Math.toRadians(ent);
+	      
+	      System.out.format("Le sinus de %.1f est : %.4f%n", ent, Math.tan(radian) );
+
+	 }
+}
