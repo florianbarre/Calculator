6290122e70b7384f62d1cd9eeb399284b2e6d838
Kiruan <alois.van@gmail.com>
2018-03-09 16:03:09 +0100

    Add function

--- Calculator/src/fr/imie/Calculator/Launcher/Scientifique.java
+++ Calculator/src/fr/imie/Calculator/Launcher/Scientifique.java
@@ -1,27 +1,29 @@
 	package fr.imie.Calculator.Launcher;
 
 public class Scientifique {
-  
-  if(src==bPuiss)
-	{
+	if(src==bPuiss){
 		aux = Double.parseDouble(txt.getText());
 		Puiss=true;
 	}
- }
-else if(src==bpuis10)
-{
-	double  x=Double.parseDouble(txt.getText());
-	double  res=Math.pow(10,x);
-	txt.setText(""+res);
-}
-else if(src==bSin)
-{
-	if(!shift)
-	{
-	double  x=Math.toRadians(Double.parseDouble(txt.getText()));
-    double res=Math.sin(x);
-	txt.setText(""+res);
+	else if(src==bpuis10){
+		double  x=Double.parseDouble(txt.getText());
+		double  res=Math.pow(10,x);
+		txt.setText(""+res);
 	}
+	else if(src==bSin){
+		if(!shift){
+			double  x=Math.toRadians(Double.parseDouble(txt.getText()));
+			double res=Math.sin(x);
+			txt.setText(""+res);
+		}
+	}
+	else if() {
+		
+	}
+	
 }
-}
-}
+
+
+
+
+
