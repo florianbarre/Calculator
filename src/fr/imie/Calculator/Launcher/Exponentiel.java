6115f92bed096d47d422fe141a76afa69185caf8
florianbarre <flobarre@outlook.fr>
2018-03-09 15:52:26 +0100

    ajout Exponentiel+calculatrice de base

---
+++ src/fr/imie/Calculator/Launcher/Exponentiel.java
@@ -0,0 +1,60 @@
+package fr.imie.Calculator.Launcher;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.Scanner;
+
+public class Exponentiel {
+	
+
+	    public static BigInteger fact(BigInteger N){
+	        BigInteger X = N;
+	        if(N.compareTo(BigInteger.ONE)<=0){
+	            return BigInteger.ONE;
+	        }else{
+	            while(N.compareTo(BigInteger.ONE)==1){
+	                X = X .multiply((N = N.subtract(BigInteger.ONE)));
+	            }
+	        }
+	        return X ;
+	    }
+
+	    public static BigDecimal exp(BigDecimal X,int n){
+	        BigDecimal sol = BigDecimal.ZERO;
+	        int i = 0;
+	        while(i<n){
+	            BigDecimal fact = new BigDecimal(fact(new BigInteger(""+i)).toString());
+	            sol = sol.add(X.pow(i).divide(fact, MathContext.DECIMAL128));
+	            i++;
+	        }
+	        return sol;
+	    }
+
+	    public static float askFloat(String message) {
+			//déclaration de variable
+			Scanner keyboard = new Scanner(System.in);
+			float result = 0;
+			boolean isOk = false;
+			
+			//récupération du float
+			while (!isOk) {
+				System.out.println(message);
+					try {
+						//instruction à faire
+						result = keyboard.nextFloat();
+						isOk = true;
+					}catch(Exception e) {
+						//instruction à faire en cas de probème
+						keyboard.nextLine();
+						isOk = false;
+					}
+			}
+			return result;
+		}
+	    
+	    public static void main(String[] args){
+	    	float e;
+	       System.out.println(Math.exp(e = askFloat("Une approximation de 'e' est :")));
+	    }
+}
---
+++ src/fr/imie/Calculator/Launcher/Launcher.java
@@ -0,0 +1,108 @@
+package fr.imie.Calculator.Launcher;
+
+import java.util.Scanner;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		float a, b, result;
+		char operation;
+		char restart = 'N';
+		
+		System.out.println("bonjour et bienvenue dans la calculatrice !");
+		
+		//Coeur du programme
+		do {
+			a = askFloat("Entrez le premier nombre !");
+			
+			do {
+				operation = askChar("Rentrez l'opérateur !");
+			}while(operation != '+' && operation != '-' && operation != '/' && operation != '*');
+			
+			b = askFloat("Entrez le deuxième nombre !");
+			
+			result = calcul(a, operation, b);
+			
+			System.out.println("Le résultat de l'opération " + a + " " + operation + " " + b + " est : " + result );
+			
+			restart = askChar("voulez-vous recommencer ? (O/Y pour recommencer) ");
+			
+		}while(restart == 'O' || restart == 'o' || restart == 'Y' || restart == 'y');		
+		
+		System.out.println("Au revoir !");
+	}
+	public static float askFloat(String message) {
+		//déclaration de variable
+		Scanner keyboard = new Scanner(System.in);
+		float result = 0;
+		boolean isOk = false;
+		
+		//récupération du float
+		while (!isOk) {
+			System.out.println(message);
+				try {
+					//instruction à faire
+					result = keyboard.nextFloat();
+					isOk = true;
+				}catch(Exception e) {
+					//instruction à faire en cas de probème
+					keyboard.nextLine();
+					isOk = false;
+				}
+		}
+		return result;
+	}
+	
+	
+	public static char askChar(String message) {
+		//déclaration de variable
+		Scanner keyboard = new Scanner(System.in);
+		char result = 0;
+		boolean isOk = false;
+		String line;
+		
+		//récupération du char
+		while (!isOk) {
+			System.out.println(message);
+				try {
+					//instruction à faire
+					line = keyboard.nextLine();
+					result = line.charAt(0);
+					isOk = true;
+				}catch(Exception e) {
+					//instruction à faire en cas de probème
+					keyboard.nextLine();
+					isOk = false;
+				}
+		}
+		return result;
+	}
+
+
+	public static float calcul(float a, char operation, float b) {
+		float result = 0;
+		
+		switch(operation) {
+		case '/':
+			if (b == 0) {
+				System.out.println("Erreur : Division par zéro !");
+				System.exit(1);
+			}
+			result = a / b;
+			break;
+		case '*':
+			result = a * b;
+			break;
+		case '-':
+			result = a - b;
+			break;
+		case '+':
+			result = a + b;
+			break;
+		default:
+			System.out.println("Erreur : opérateur inconnu !");
+		}
+		return result;
+		
+	}
+	}
